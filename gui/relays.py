cd07b79212705ff90a84676bd0d9f2a2e16818a1
Andreas Wansner <andreas@anwa-soft.de>
2025-06-26 15:26:27 +0200

    Fixed handling of non number payloads like relay status ON/OFF Removed old code

--- gui/relays.py
+++ gui/relays.py
@@ -33,32 +33,12 @@ class RelaysView(BoxLayout):
         Clock.schedule_once(lambda dt: self._update_value(name, value))
 
     def _update_value(self, name, value):
-        setattr(self, name, str(value))
-
-    """
-    def on_mqtt_message(self, client, userdata, msg):
-        if msg.topic == "GTN/Pool/Pumpe/tele/SENSOR":
-            try:
-                data = json.loads(msg.payload.decode())
-                # ENERGY kann fehlen, daher absichern:
-                energy = data.get("ENERGY", {})
-                power = energy.get("Power")
-                if power is not None:
-                    logger.info(f"Aktuelle Pumpenleistung: {power} W")
-                    # In Kivy-Property schreiben
-                    self.pumpe_power = f"{power} W"
-                else:
-                    logger.info("Power-Wert nicht gefunden!")
-                    self.pumpe_power = "?"
-            except Exception as e:
-                logger.info(f"Fehler beim Parsen: {e}")
-                self.pumpe_power = "?"
-
-    def toggle_relay(self, relay_name, state):
-        # Hier kommt deine MQTT-Logik hin
-        print(f"Relais {relay_name} auf {state} gesetzt")
-        setattr(self, f"{relay_name}_state", state)
-    """
+        if value == "ON":
+            setattr(self, name, True)
+        elif value == "OFF":
+            setattr(self, name, False)
+        else:
+            setattr(self, name, str(value))
 
     def toggle_relay(self, relay_name, state):
         cmd = "ON" if state else "OFF"
