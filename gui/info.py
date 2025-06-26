af585030dd0b83991509ba28f96b28ad6c9a64bc
Andreas Wansner <andreas@anwa-soft.de>
2025-06-26 20:52:15 +0200

    Add other system sensors

--- gui/info.py
+++ gui/info.py
@@ -2,21 +2,29 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, BooleanProperty
 from kivy.lang import Builder
 from kivy.clock import Clock
-from sensors.system import read_cpu_temperature
+from sensors.system import read_cpu_temperature, read_cpu_usage, read_memory_usage, read_sd_card_usage
 
 Builder.load_file("gui/info.kv")
 
-
 class InfoPage(BoxLayout):
     cpu_temp = StringProperty("0")
+    cpu_use  = StringProperty("0")
+    mem_use  = StringProperty("0")
+    sd_use   = StringProperty("0")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.cpu_temp = str(read_cpu_temperature())
+        self.cpu_use = str(read_cpu_usage())
+        self.mem_use = str(read_memory_usage())
+        self.sd_use = str(read_sd_card_usage())
 
         # alle 10 Sekunden aktualisieren
         Clock.schedule_interval(self.update, 10)
 
     def update(self, dt):
         self.cpu_temp = str(read_cpu_temperature())
+        self.cpu_use = str(read_cpu_usage())
+        self.mem_use = str(read_memory_usage())
+        self.sd_use = str(read_sd_card_usage())
 
--- sensors/system.py
+++ sensors/system.py
@@ -1,5 +1,7 @@
 from utils.logger import logger
+import psutil
 import os
+import subprocess
 
 def read_cpu_temperature():
     try:
@@ -10,4 +12,37 @@ def read_cpu_temperature():
         return temp_celsius
     except Exception as e:
         logger.error(f"Fehler beim Auslesen der CPU-Temperatur: {e}")
+        return "n.a."
+
+def read_cpu_usage(self):
+    try:
+        usage = psutil.cpu_percent(interval=1)
+        logger.debug(f"CPU-Auslastung: {usage:.1f}%")
+        return usage
+    except Exception as e:
+        logger.error(f"Fehler beim Auslesen der CPU-Auslastung: {e}")
+        return "n.a."
+
+def read_memory_usage(self):
+    try:
+        mem = psutil.virtual_memory()
+        usage_percent = mem.percent
+        logger.debug(f"RAM-Nutzung: {usage_percent:.1f}%")
+        return usage_percent
+    except Exception as e:
+        logger.error(f"Fehler beim Auslesen der RAM-Nutzung: {e}")
+        return "n.a."
+
+def read_sd_card_usage(self):
+    try:
+        result = subprocess.check_output(["df", "-h", "/"]).decode()
+        for line in result.splitlines():
+            if "/dev/root" in line or "/" in line:
+                usage_percent = float(line.split()[4].rstrip("%"))
+                logger.debug(f"SD-Karten-Nutzung: {usage_percent:.1f}%")
+                return usage_percent
+        logger.warning("Keine SD-Karten-Informationen gefunden")
+        return "n.a."
+    except Exception as e:
+        logger.error(f"Fehler beim Auslesen der SD-Karten-Nutzung: {e}")
         return "n.a."
