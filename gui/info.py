c0d24fae8f18ee2c956798f37b96053b354ead98
Andreas Wansner <andreas@anwa-soft.de>
2025-06-26 18:09:47 +0200

    Add cpu temperature as first sensor

--- gui/info.py
+++ gui/info.py
@@ -1,8 +1,22 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, BooleanProperty
 from kivy.lang import Builder
+from kivy.clock import Clock
+from sensors.system import read_cpu_temperature
 
 Builder.load_file("gui/info.kv")
 
 
 class InfoPage(BoxLayout):
-    pass
+    cpu_temp = StringProperty("0")
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cpu_temp = read_cpu_temperature()
+
+        # alle 10 Sekunden aktualisieren
+        Clock.schedule_interval(self.update, 10)
+
+    def update(self, dt):
+        self.cpu_temp = str(read_cpu_temperature())
+
